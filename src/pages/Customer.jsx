179a67e0dde869a2e053c1e5335a030659796794
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-16 22:54:18 +0530

    feat: customer

--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { ColumnDirective, ColumnsDirective, Edit, Filter, GridComponent, Inject, Page, Selection, Sort, Toolbar } from '@syncfusion/ej2-react-grids';
+import React from 'react';
 
-const Customer = () => {
+import { Header } from '../components';
+import { customersData, customersGrid } from '../data/dummy';
+
+const Customers = () => {
+    const selectionsettings = { persistSelection: true };
+    const toolbarOptions = ['Delete'];
+    const editing = { allowDeleting: true, allowEditing: true };
+
     return (
-        <div>Customer</div>
-    )
-}
+        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <Header category="Page" title="Customers" />
+        <GridComponent
+            dataSource={customersData}
+            enableHover={false}
+            allowPaging
+            pageSettings={{ pageCount: 5 }}
+            selectionSettings={selectionsettings}
+            toolbar={toolbarOptions}
+            editSettings={editing}
+            allowSorting
+        >
+            <ColumnsDirective>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+            </ColumnsDirective>
+            <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
+        </GridComponent>
+        </div>
+    );
+};
 
-export default Customer
+export default Customers;
