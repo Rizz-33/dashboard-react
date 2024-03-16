a2d148718127c403f60814ad1d4689ab7a88abdb
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-16 19:58:17 +0530

    modify: index[components]

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,7 @@
 export { default as Button } from './Button';
 export { default as Sidebar } from './Sidebar';
 export { default as ThemeSettings } from './ThemeSettings';
-// eslint-disable-next-line import/no-cycle
+
 export { default as Cart } from './Cart';
 export { default as ChartsHeader } from './ChartsHeader';
 export { default as Chat } from './Chat';
@@ -14,3 +14,4 @@ export { default as LineChart } from './charts/LineChart';
 export { default as Pie } from './charts/Pie';
 export { default as SparkLine } from './charts/SparkLine';
 export { default as Stacked } from './charts/Stacked';
+
