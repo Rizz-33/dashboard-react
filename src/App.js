c5b4ff57f63450a6ef930b1095fa455734abd6e5
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-16 18:59:49 +0530

    modify: app.js [display settings icon]

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import React from 'react';
+import { FiSettings } from 'react-icons/fi';
+import { BrowserRouter } from 'react-router-dom';
+
 import './App.css';
 
 const App = () => {
     return (
-        <h1 className='underline text-3xl'>App</h1>
+        <div>
+            <BrowserRouter>
+                <div className='flex relative dark:bg-main-dark-bg'>
+                    <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
+                        <TooltipComponent content='Settings' position='Top'>
+                            <button>
+                                <FiSettings />
+                            </button>
+                        </TooltipComponent>
+                    </div>
+                </div>
+            </BrowserRouter>
+        </div>
     )
 }
 
